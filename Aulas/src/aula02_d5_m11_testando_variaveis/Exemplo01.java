772b79311e22e5817cac47d17be86e6dc191444e
VitorMotaNJ <vitormotanovais@gmail.com>
2025-11-05 10:15:59 -0300

    Atualizando aula2

---
+++ Aulas/src/aula02_d5_m11_testando_variaveis/Exemplo01.java
@@ -0,0 +1,15 @@
+package aula02_d5_m11_testando_variaveis;
+
+public class Exemplo01 {
+    public class Exemplo_1{
+        public static void main(String[] args){
+
+            int numero;
+            String nome;
+            float altura;
+
+        }
+    }
+}
+
+
---
+++ Aulas/src/aula02_d5_m11_testando_variaveis/Exemplo02.java
@@ -0,0 +1,12 @@
+package aula02_d5_m11_testando_variaveis;
+
+public class Exemplo02 {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+
+        int soma = n1 + n2;
+
+        System.out.println("A soma dos numeros é: "+soma);
+    }
+}
