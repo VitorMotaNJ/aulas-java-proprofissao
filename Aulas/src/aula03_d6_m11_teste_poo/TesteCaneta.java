e3a4619ba018c6f4a04dd5994a33c421b70d5987
VITOR MOTA NOVAIS DE JESUS <vitor.mnjesus@senacsp.edu.br>
2025-11-06 12:27:00 -0300

    Criando classe principal, e instanciando objeto caneta da classe Caneta

--- Aulas/src/aula03_d6_m11_teste_poo/TesteCaneta.java
+++ Aulas/src/aula03_d6_m11_teste_poo/TesteCaneta.java
@@ -1,4 +1,16 @@
 package aula03_d6_m11_teste_poo;
 
 public class TesteCaneta {
+    public static void main(String[] args) {
+        Caneta caneta1 = new Caneta();
+        caneta1.modelo = "esferográfica";
+        caneta1.cor = "azul";
+        caneta1.ponta = 0.5f;
+        caneta1.carga = 90;
+        caneta1.tampada = true;
+
+        caneta1.estado();
+        caneta1.rabiscar();
+
+    }
 }
