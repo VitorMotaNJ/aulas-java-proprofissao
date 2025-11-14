0a76060982c4f669d377e75f8923e2423cd241c1
vitor-mota <vitormotanovais@gmail.com>
2025-11-14 11:08:53 -0300

    Adicionando classe para praticar o laço de repetição while em java

---
+++ Aulas/src/aula03_d14_m11_comandos_repeticao/TesteWhile.java
@@ -0,0 +1,11 @@
+package aula03_d14_m11_comandos_repeticao;
+
+public class TesteWhile {
+    static void main(String[] args) {
+        int n = 1;
+        while(n <= 10){
+            System.out.println("n = " + n);
+            n++;
+        }
+    }
+}
