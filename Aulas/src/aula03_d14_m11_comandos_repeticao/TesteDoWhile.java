853b4bbf01c99629df644792cc4407447439b251
vitor-mota <vitormotanovais@gmail.com>
2025-11-14 11:12:45 -0300

    adiciando pratica de laço de repetiçao Do While em java

---
+++ Aulas/src/aula03_d14_m11_comandos_repeticao/TesteDoWhile.java
@@ -0,0 +1,11 @@
+package aula04_d14_m11_comandos_repeticao;
+
+public class TesteDoWhile {
+    static void main(String[] args) {
+        int n = 1;
+        do{
+            System.out.println("n = " + n);
+            n++;
+        }while(n <= 10);
+    }
+}
