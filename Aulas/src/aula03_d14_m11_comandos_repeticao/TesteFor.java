86617cb3fd74dc33f0cc66458cda44b0a33fbc1e
vitor-mota <vitormotanovais@gmail.com>
2025-11-14 11:06:06 -0300

    Adicionando arquivo classe de teste de laço for em java

--- Aulas/src/aula03_d14_m11_comandos_repeticao/TesteFor.java
+++ Aulas/src/aula03_d14_m11_comandos_repeticao/TesteFor.java
@@ -1,4 +1,10 @@
 package aula03_d14_m11_comandos_repeticao;
 
 public class TesteFor {
+    static void main(String[] args) {
+        int n = 0;
+        for(n = 1; n <= 10; n++){
+            System.out.println("n = " + n);
+        }
+    }
 }
