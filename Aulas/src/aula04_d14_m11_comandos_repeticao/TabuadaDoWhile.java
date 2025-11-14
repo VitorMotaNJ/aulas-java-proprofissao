922372e85beb16626ee2f8bfc0dfa40cbe7d79c3
vitor-mota <vitormotanovais@gmail.com>
2025-11-14 11:38:58 -0300

    Adicionando pratica de tabuada com laço de repetição DoWhile em java

---
+++ Aulas/src/aula04_d14_m11_comandos_repeticao/TabuadaDoWhile.java
@@ -0,0 +1,21 @@
+package aula04_d14_m11_comandos_repeticao;
+
+import java.util.Scanner;
+
+public class TabuadaDoWhile {
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-----------------TABUADA-----------------");
+        System.out.println("Digite o numero que você deseja ver a tabuada:");
+        int numero = sc.nextInt();
+        System.out.println("TABUADA DE " + numero + "");
+        int i = 0;
+        do {
+            System.out.println(numero + " x " + i + " = " + (numero * i) + "");
+            i++;
+        }while (i <= 10);
+
+    }
+}
